9a39911f4e71efa54a77d4d571996f8766d87157
dondavan <yeezushugh@gmail.com>
2025-07-17 11:50:16 +0200

    cluster

--- export_model/acl/scp_data_2_device.sh
+++ export_model/acl/scp_data_2_device.sh
@@ -1,2 +1,2 @@
-scp -r ./data khadas@Khadas.local:/home/khadas/acl
-
+sshpass -p 'khadas' scp -r ./data khadas@Khadas.local:/home/khadas/acl
+sshpass -p 'khadas' scp -r ./script/* khadas@Khadas.local:/home/khadas/acl
