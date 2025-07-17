3b4037224703b0c406ef00889814ae0a27e80ae5
dondavan <yeezushugh@gmail.com>
2025-07-17 13:23:33 +0200

    replace center

---
+++ export_model/acl/script/run_bert_cpu_cluster.sh
@@ -0,0 +1,6 @@
+export LD_LIBRARY_PATH=lib/
+
+d_model=768
+input_len='7'
+./graph_bert_base_uncased --d_model=$d_model --threads=4 --input_len=${input_len} --text=./data/bert-clustered/input/input_text.txt --segment=./data/bert-clustered/input/input_segment.txt --data=./data/bert-clustered/ --vocabulary=./data/bert-clustered/vocab.txt --raw-output=true
+rm measure_output.txt
